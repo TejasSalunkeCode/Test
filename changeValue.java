1aa1a66f181ed17c1147b4ad48d12d780180457f
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-01-27 10:37:07 +0530

    add commit

--- changeValue.java
+++ changeValue.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 
 public class changeValue {
     public static void main(String[] args) {
+        System.out.println("hello world");
         int []arr={1,3,7,85,77};
         change(arr);
         System.out.println(Arrays.toString(arr));
